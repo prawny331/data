7abd9dd7bc714aac23725a0a4401134b728f7354
smal <6433336+smaldragon@users.noreply.github.com>
2022-10-10 12:11:14 +0100

    ascii tree output

--- parse.py
+++ parse.py
@@ -4,6 +4,9 @@ import time
 from os import environ
 from style import PRESENTATIONS, GREY
 
+from asciitree import LeftAligned
+from asciitree.drawing import BoxStyle, BOX_ASCII, BOX_DOUBLE, BOX_BLANK, BOX_HEAVY, BOX_LIGHT
+
 re_coord = re.compile("\((.*?)\)")
 re_color = re.compile("\<(#.{6})\>")
 re_comment = re.compile("#.*")
@@ -11,6 +14,8 @@ re_comment = re.compile("#.*")
 features = []
 dest_stack = []
 dest_list = []
+dest_tree = {"/dest !":{}}
+dest_nocoord = []
 
 # Read OneDest tree
 with open(r'tree.txt') as f:
@@ -19,9 +24,9 @@ with open(r'tree.txt') as f:
   for line in f.readlines():
     ident=0
     striped = line.strip()
-    name = striped.split(" ")[0]
+    dest_name = striped.split(" ")[0]
     
-    # Parse /dest
+    # Parse tree position
     dest = "/dest !"
     for c in line:
         if c in (" ","\t"):
@@ -33,18 +38,18 @@ with open(r'tree.txt') as f:
         if ident <= item[1]:
             dest_stack.pop(i)
     
-    dest_name = name.lower()
+    dest_id = dest_name.lower()
     
-    if dest_name in dest_list:
-        raise Exception(f"ERROR: /dest {name} is present multiple times on the tree")
+    if dest_id in dest_list:
+        raise Exception(f"ERROR: /dest {dest_name} is present multiple times on the tree")
     
-    dest_list.append(dest_name)
-    dest_stack.append((dest_name,ident))
+    dest_list.append(dest_id)
+    dest_stack.append((dest_id,ident))
     level = len(dest_stack)
     
     if level == 2:
         color = GREY
-    
+
     # Parse Coordinates
     x,y,z = None,None,None
     coord_parse = re_coord.search(striped)
@@ -57,7 +62,9 @@ with open(r'tree.txt') as f:
             x = int(coords[0])
             y = int(coords[1])
             z = int(coords[2])
-        
+    elif level > 1:
+        dest_nocoord.append(dest_id)
+  
     # Parse color
     color_parse = re_color.search(striped)
     if color_parse:
@@ -69,11 +76,21 @@ with open(r'tree.txt') as f:
     if comment_parse:
         comment = comment_parse.group(0)[1:]
     
+    # Generate /dest command
+    cur_item = dest_tree["/dest !"]
     for i in dest_stack:
-        dest+=f" {i[0]}"
+        n = i[0]
+        dest+=f" {n}"
+        
+        if n in dest_nocoord:
+            n+="*"
+        if not n in cur_item:
+            cur_item[n] = dict()
+        cur_item = cur_item[n]
+    
     # Add to ccmap feature list
     if x is not None and z is not None:
-        o={'name':name,'x':x,'z':z,'dest':dest,'level':level,'id':'civmap:onedest/station/'+name.lower()}
+        o={'name':dest_name,'x':x,'z':z,'dest':dest,'level':level,'id':'civmap:onedest/station/'+dest_id}
         if y is not None:
             o['y'] = y
         if comment is not None:
@@ -102,3 +119,23 @@ print(collection_string)
 
 with open('civmap.json','w') as f:
     f.write(collection_string)
+
+# Generate ASCII representation
+sbar = "─"
+stree = "┬"
+spipe = "│"
+sfinal = "└"
+
+tr = LeftAligned(
+    draw = BoxStyle(
+        gfx=BOX_LIGHT,
+        ident=2,
+    )
+)
+
+o = tr(dest_tree)
+o += "\n\n* lacks a known station"
+print(o)
+
+with open('tree_ascii.txt','w') as f:
+    f.write(o)
